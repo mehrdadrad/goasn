08ae8d36225a3ab06528e8327fb0aea88c0f3db2
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2017-11-22 17:56:51 -0800

    added dump method and more code for testing

--- goasn.go
+++ goasn.go
@@ -2,12 +2,24 @@ package goasn
 
 import (
 	"bufio"
+	"bytes"
+	"compress/gzip"
+	"encoding/gob"
+	"io"
+	"log"
 	"net/http"
+	"os"
 	"regexp"
+	"strconv"
 )
+
+type ASNInfo struct {
+	Descr string
+}
 
 type ASNReference struct {
 	URL     string
+	Data    map[uint64]ASNInfo
 	Offline bool
 }
 
@@ -17,7 +29,7 @@ func NewASN() *ASNReference {
 	}
 }
 
-func (a *ASNReference) getDataURL() (map[string]string, error) {
+func (a *ASNReference) getDataURL() (map[uint64]ASNInfo, error) {
 	resp, err := http.Get(a.URL)
 	if err != nil {
 		return nil, err
@@ -28,12 +40,14 @@ func (a *ASNReference) getDataURL() (map[string]string, error) {
 	re := regexp.MustCompile(">AS(\\d+)\\s*</a>\\s*(.*)\\s*")
 
 	scanner := bufio.NewScanner(resp.Body)
-	result := make(map[string]string)
+	result := make(map[uint64]ASNInfo)
 
 	for scanner.Scan() {
 		asn := re.FindStringSubmatch(scanner.Text())
 		if len(asn) == 3 {
-			result[asn[1]] = asn[2]
+			if num, err := strconv.ParseUint(asn[1], 10, 64); err == nil {
+				result[num] = ASNInfo{Descr: asn[2]}
+			}
 		}
 	}
 
@@ -42,4 +56,52 @@ func (a *ASNReference) getDataURL() (map[string]string, error) {
 	}
 
 	return result, nil
+}
+
+func (a *ASNReference) Init() {
+	r, _ := a.getDataURL()
+	a.Data = r
+
+	fh, err := os.Create("./goasn.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := a.dump(fh); err != nil {
+		log.Fatal(err)
+	}
+
+	fh.Close()
+}
+
+func (a *ASNReference) Get(asn uint64) ASNInfo {
+	return a.Data[asn]
+}
+
+func (a *ASNReference) load(r io.Reader) error {
+	zh, err := gzip.NewReader(r)
+	if err != nil {
+		return err
+	}
+
+	_ = zh
+
+	return nil
+}
+
+func (a *ASNReference) dump(w io.Writer) error {
+	var buf = new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+	if err := enc.Encode(a.Data); err != nil {
+		return err
+	}
+
+	zh := gzip.NewWriter(w)
+	if _, err := zh.Write(buf.Bytes()); err != nil {
+		return err
+	}
+
+	zh.Close()
+
+	return nil
 }
