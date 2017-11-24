5f42b1973cadccaaa259713f4678752f93d86e5c
Mehrdad Arshad Rad <arshad.rad@gmail.com>
2017-11-24 10:17:59 -0800

    added load test

--- goasn_test.go
+++ goasn_test.go
@@ -1,6 +1,9 @@
 package goasn
 
 import (
+	"bytes"
+	"compress/gzip"
+	"encoding/gob"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
@@ -26,5 +29,33 @@ func TestGetDataURL(t *testing.T) {
 		t.Error("Expect to have AS792 but not exit")
 	} else if asn.Descr != "ORACLE-ASNBLOCK-ASN - Oracle Corporation, US" {
 		t.Errorf("Expect to have AS792 description but it has %s", asn.Descr)
+	}
+}
+
+func TestLoad(t *testing.T) {
+	asn := NewASN()
+	data := map[uint64]ASNInfo{15133: ASNInfo{Descr: "EdgeCast"}}
+
+	buf := new(bytes.Buffer)
+	enc := gob.NewEncoder(buf)
+	if err := enc.Encode(data); err != nil {
+		t.Fatal(err)
+	}
+
+	w := new(bytes.Buffer)
+	zh := gzip.NewWriter(w)
+	if _, err := zh.Write(buf.Bytes()); err != nil {
+		t.Fatal(err)
+	}
+
+	zh.Close()
+
+	res, err := asn.load(w)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if _, ok := res[15133]; !ok {
+		t.Error("expected to have ASN 15133, but not exist!")
 	}
 }
